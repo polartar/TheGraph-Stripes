dea5d8581a6f0734b954107a9e6246e5a905c664
Richard Francis <rich@rfrancis.io>
2021-06-07 20:33:44 +0100

    Latest UI bits

---
+++ src/components/LiveSignum/index.tsx
@@ -0,0 +1,59 @@
+import style from './style.module.scss';
+import {
+  faCaretDown,
+  faCaretUp,
+  faCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classes from 'utils/classes';
+
+interface Props {
+  value: string;
+  className?: string;
+}
+
+const LiveSignum = ({ value, className }: Props) => {
+  const lastValue = useRef(parseFloat(value));
+  const [direction, setDirection] = useState<'up' | 'down'>();
+
+  useEffect(() => {
+    const floatVal = parseFloat(value);
+    console.log(floatVal);
+
+    if (floatVal > lastValue.current) {
+      setDirection('up');
+    } else if (floatVal < lastValue.current) {
+      setDirection('down');
+    }
+
+    lastValue.current = floatVal;
+  }, [value]);
+
+  if (direction === 'up') {
+    return (
+      <span className={classes(style.positive, className)}>
+        <FontAwesomeIcon icon={faCaretUp} className={style.icon} />
+        {value}
+      </span>
+    );
+  }
+
+  if (direction === 'down') {
+    return (
+      <span className={classes(style.negative, className)}>
+        <FontAwesomeIcon icon={faCaretDown} className={style.icon} />
+        {value}
+      </span>
+    );
+  }
+
+  return (
+    <span className={className}>
+      <FontAwesomeIcon icon={faCaretUp} className={style.icon} />
+      {value}
+    </span>
+  );
+};
+
+export default LiveSignum;
--- src/components/form/MarketSelect/index.tsx
+++ src/components/form/MarketSelect/index.tsx
@@ -1,7 +1,9 @@
 import { faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LiveSignum from 'components/LiveSignum';
 import MarketName from 'components/MarketName';
 import useModal from 'hooks/useModal';
+import { useEffect, useState } from 'react';
 import classes from 'utils/classes';
 import Menu from './Menu';
 import style from './style.module.scss';
@@ -11,7 +13,20 @@ interface Props {
 }
 
 const MarketSelect = ({ className }: Props) => {
+  const [marketRate, setMarketRate] = useState('6.54%');
+  const [oracleRate, setOracleRate] = useState('14.32%');
   const modal = useModal();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMarketRate(`${(6 + randomNumber()).toFixed(2)}%`);
+      setOracleRate(`${(12 + randomNumber()).toFixed(2)}%`);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
 
   function openMenu() {
     modal.present(<Menu />);
@@ -25,10 +40,24 @@ const MarketSelect = ({ className }: Props) => {
           <FontAwesomeIcon icon={faAngleRight} />
           USDC Deposit Rate
         </div>
+        <div className={style.rates}>
+          <div className={style.rate}>
+            <span className={style.rateLabel}>Oracle (Floating) Rate</span>
+            <LiveSignum value={oracleRate} className={style.rateAmount} />
+          </div>
+          <div className={style.rate}>
+            <span className={style.rateLabel}>Market (Fixed) Rate</span>
+            <LiveSignum value={marketRate} className={style.rateAmount} />
+          </div>
+        </div>
       </div>
       <FontAwesomeIcon icon={faAngleDown} className={style.caret} />
     </div>
   );
 };
+
+function randomNumber() {
+  return Math.floor(Math.random() * (1000 - 100) + 100) / 100;
+}
 
 export default MarketSelect;
