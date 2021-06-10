82b59fef165cb335bf195ab234c07c6465bc5f04
talentdev2020 <polaris.center@outlook.com>
2021-06-10 12:04:04 -0700

    implement liquidity tabs

--- src/views/Liquidity/index.tsx
+++ src/views/Liquidity/index.tsx
@@ -1,6 +1,24 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import style from './style.module.scss';
+
 const Liquidity = () => {
   return (
-    <div>Liquidity</div>
+    <div className={style.panel}> 
+      <Tabs className='tabs'>
+        <TabList>
+          <Tab>Trade</Tab>
+          <Tab>Liquidity</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
   )
 }
 
